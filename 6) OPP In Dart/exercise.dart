17bfafe0eef99238f2c537312cc7d0059b22ac53
Zuraiz Khan <137849379+thezuraiz@users.noreply.github.com>
2023-10-25 19:04:34 +0500

    Update exercise.dart

--- 6) OPP In Dart/exercise.dart
+++ 6) OPP In Dart/exercise.dart
@@ -59,3 +59,128 @@ void main() {
   house3.PrintAll();
 }
 
+// 3) Write a dart program to create an enum class for gender [male, female, others] and print all values.
+enum Gender {
+  Male,
+  Female,
+  Others,
+}
+
+void main() {
+  List<Gender> genders = Gender.values;
+
+  print("All Gender Values:");
+  for (var gender in genders) {
+    print(gender.toString());
+  }
+}
+
+// 4) Write a dart program to create a class Animal with properties [id, name, color]. Create another class called Cat and extends it from Animal. Add new properties sound in String. Create an object of a Cat and print all details.
+class Animal {
+  int id;
+  String name;
+  String color;
+
+  Animal(this.id, this.name, this.color);
+}
+
+class Cat extends Animal {
+  String sound;
+
+  Cat(int id, String name, String color, this.sound) : super(id, name, color);
+}
+
+void main() {
+  Cat myCat = Cat(1, "cato", "Gray", "Meoow");
+
+  print("Cat Details:");
+  print("ID: ${myCat.id}");
+  print("Name: ${myCat.name}");
+  print("Color: ${myCat.color}");
+  print("Sound: ${myCat.sound}");
+}
+
+// 5) Write a dart program to create a class Camera with private properties [id, brand, color, prize]. Create a getter and setter to get and set values. Also, create 3 objects of it and print all details.
+class Camera {
+  int _id;
+  String _brand;
+  String _color;
+  double _price;
+
+  Camera(this._id, this._brand, this._color, this._price);
+
+  int get id => _id;
+  String get brand => _brand;
+  String get color => _color;
+  double get price => _price;
+
+  set id(int id) {
+    if (id > 0) {
+      _id = id;
+    }
+  }
+
+  set brand(String brand) {
+    if (brand.isNotEmpty) {
+      _brand = brand;
+    }
+  }
+
+  set color(String color) {
+    if (color.isNotEmpty) {
+      _color = color;
+    }
+  }
+
+  set price(double price) {
+    if (price > 0) {
+      _price = price;
+    }
+  }
+}
+
+void main() {
+  Camera camera1 = Camera(1, "Canon", "Black", 799.99);
+  Camera camera2 = Camera(2, "Nikon", "Silver", 899.99);
+  Camera camera3 = Camera(3, "Sony", "Red", 699.99);
+
+  print("Camera 1 Details:");
+  print("ID: ${camera1.id}");
+  print("Color: ${camera1.color}");
+  print("Price: \$${camera1.price}");
+
+  print("\nCamera 2 Details:");
+  print("ID: ${camera2.id}");
+  print("Brand: ${camera2.brand}");
+  print("Price: \$${camera2.price}");
+
+  print("\nCamera 3 Details:");
+  print("ID: ${camera3.id}");
+  print("Brand: ${camera3.brand}");
+  print("Color: ${camera3.color}");
+  print("Price: \$${camera3.price}");
+}
+
+// 6) Create an interface called Bottle and add a method to it called open(). Create a class called CokeBottle and implement the Bottle and print the message “Coke bottle is opened”. Add a factory constructor to Bottle and return the object of CokeBottle. Instantiate CokeBottle using the factory constructor and call the open() on the object.
+
+class Bottle {
+  void open();
+}
+
+class CokeBottle implements Bottle {
+  @override
+  void open() {
+    print("Coke bottle is opened");
+  }
+}
+
+class BottleFactory {
+  factory BottleFactory.cokeBottle() {
+    return CokeBottle();
+  }
+}
+
+void main() {
+  Bottle cokeBottle = BottleFactory.cokeBottle();
+  cokeBottle.open();
+}
